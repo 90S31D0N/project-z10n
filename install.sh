86da034a4df449f38ee10baabfb9f2babb4d5150
90S31D0N <noelschaller32@outlook.com>
2025-12-11 22:35:18 +0100

    add sudo rights

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 # This script installs the necessary dependencies for the project.
-./install_scripts/install_paru.sh
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run this script with sudo"
+    exit 1
+fi
+sudo ./install_scripts/install_paru.sh
--- install_scripts/install_paru.sh
+++ install_scripts/install_paru.sh
@@ -2,6 +2,6 @@
 
 git clone https://aur.archlinux.org/paru.git /tmp/paru
 cd /tmp/paru
-makepkg -si --noconfirm
+sudo makepkg -si --noconfirm
 cd -
 rm -rf /tmp/paru

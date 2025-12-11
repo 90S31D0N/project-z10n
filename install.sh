988e9b02ba9f29fc861fe543acb2ec5b750c2683
90S31D0N <noelschaller32@outlook.com>
2025-12-11 22:40:12 +0100

    test

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# This script installs the necessary dependencies for the project.
-if [ "$EUID" -ne 0 ]; then
-    echo "Please run this script with sudo"
-    exit 1
-fi
+# # This script installs the necessary dependencies for the project.
+# if [ "$EUID" -ne 0 ]; then
+#     echo "Please run this script with sudo"
+#     exit 1
+# fi
 
 ./install_scripts/install_paru.sh
